8f7deab655b7c0c072d9677aa4a9fdbfa337679f
FernandoDiasH <fernandohiroshi10@hotmail.com>
2022-12-05 20:32:43 -0300

    Finalizando menu superior

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="h-48 bg-black bg-gradient-to-t from-gray-700 ">
+    <div class="h-56 bg-black bg-gradient-to-t from-gray-700 ">
         <nav class=" grid grid-cols-4 content-center h-24">
             <NuxtLink to="/" class="col-span-2 ml-4" >
                 <img  src="../assets/image/logo_branca_header.svg" alt="Logotipo induspharma">
@@ -12,46 +12,39 @@
                         <MenuItems class="absolute  bg-white text-black rounded-b w-60 border-t-4 border-blue-500">
                             <div class="flex flex-col">
                                 <MenuItem  class="border hover:border-blue-500 p-2 group">
-                                    <div class="flex items-end justify-between">
-                                        <div class="flex flex-col justify-between">
-                                            <svg class="mb-6" width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path class="group-hover:fill-blue-500" fill-rule="evenodd" clip-rule="evenodd" d="M9 18.5C13.9706 18.5 18 14.4706 18 9.5C18 4.52944 13.9706 0.5 9 0.5C4.02944 0.5 0 4.52944 0 9.5C0 14.4706 4.02944 18.5 9 18.5ZM9.00343 16.2832C8.06867 16.2832 7.3109 15.5254 7.3109 14.5906V11.6386H4.33963C3.40488 11.6386 2.64711 10.8808 2.64711 9.94608C2.64711 9.01132 3.40488 8.25355 4.33963 8.25355H7.3109V4.85859C7.3109 3.92384 8.06867 3.16607 9.00343 3.16607C9.93819 3.16607 10.696 3.92384 10.696 4.85859V8.25355H14.0717C15.0064 8.25355 15.7642 9.01132 15.7642 9.94608C15.7642 10.8808 15.0064 11.6386 14.0717 11.6386H10.696V14.5906C10.696 15.5254 9.93819 16.2832 9.00343 16.2832Z" fill="#B7B7B7"/>
-                                            </svg>
-                                            <NuxtLink to="/products">Acelerador Biologico</NuxtLink>
+                                    <NuxtLink to="/products">
+                                        <div class="flex items-end justify-between">
+                                            <div class="flex flex-col justify-between">
+                                                <svg class="mb-6" width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path class="group-hover:fill-blue-500" fill-rule="evenodd" clip-rule="evenodd" d="M9 18.5C13.9706 18.5 18 14.4706 18 9.5C18 4.52944 13.9706 0.5 9 0.5C4.02944 0.5 0 4.52944 0 9.5C0 14.4706 4.02944 18.5 9 18.5ZM9.00343 16.2832C8.06867 16.2832 7.3109 15.5254 7.3109 14.5906V11.6386H4.33963C3.40488 11.6386 2.64711 10.8808 2.64711 9.94608C2.64711 9.01132 3.40488 8.25355 4.33963 8.25355H7.3109V4.85859C7.3109 3.92384 8.06867 3.16607 9.00343 3.16607C9.93819 3.16607 10.696 3.92384 10.696 4.85859V8.25355H14.0717C15.0064 8.25355 15.7642 9.01132 15.7642 9.94608C15.7642 10.8808 15.0064 11.6386 14.0717 11.6386H10.696V14.5906C10.696 15.5254 9.93819 16.2832 9.00343 16.2832Z" fill="#B7B7B7"/>
+                                                </svg>
+                                                <p>Acelerador Biologico</p>
+                                            </div>
+                                            <img src="../assets/image/amf_menu.svg" alt="">
                                         </div>
-                                        <img src="../assets/image/amf_menu.svg" alt="">
-                                    </div>
+                                    </NuxtLink>
                                 </MenuItem>
-                                <MenuItem  class="border hover:border-blue-500 p-2 group">
-                                    <div class="flex items-end justify-between">
-                                        <div class="flex flex-col justify-between">
-                                            <svg class="mb-6" width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path class="group-hover:fill-blue-500" fill-rule="evenodd" clip-rule="evenodd" d="M9 18.5C13.9706 18.5 18 14.4706 18 9.5C18 4.52944 13.9706 0.5 9 0.5C4.02944 0.5 0 4.52944 0 9.5C0 14.4706 4.02944 18.5 9 18.5ZM9.00343 16.2832C8.06867 16.2832 7.3109 15.5254 7.3109 14.5906V11.6386H4.33963C3.40488 11.6386 2.64711 10.8808 2.64711 9.94608C2.64711 9.01132 3.40488 8.25355 4.33963 8.25355H7.3109V4.85859C7.3109 3.92384 8.06867 3.16607 9.00343 3.16607C9.93819 3.16607 10.696 3.92384 10.696 4.85859V8.25355H14.0717C15.0064 8.25355 15.7642 9.01132 15.7642 9.94608C15.7642 10.8808 15.0064 11.6386 14.0717 11.6386H10.696V14.5906C10.696 15.5254 9.93819 16.2832 9.00343 16.2832Z" fill="#B7B7B7"/>
-                                            </svg>
-                                            <NuxtLink to="/products">Acelerador Biologico</NuxtLink>
-                                        </div>
-                                        <img src="../assets/image/amf_menu.svg" alt="">
-                                    </div>
-                                </MenuItem>
-                                <MenuItem  class="border hover:border-blue-500 p-2 group">
-                                    <div class="flex items-end justify-between">
-                                        <div class="flex flex-col justify-between">
-                                            <svg class="mb-6" width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path class="group-hover:fill-blue-500" fill-rule="evenodd" clip-rule="evenodd" d="M9 18.5C13.9706 18.5 18 14.4706 18 9.5C18 4.52944 13.9706 0.5 9 0.5C4.02944 0.5 0 4.52944 0 9.5C0 14.4706 4.02944 18.5 9 18.5ZM9.00343 16.2832C8.06867 16.2832 7.3109 15.5254 7.3109 14.5906V11.6386H4.33963C3.40488 11.6386 2.64711 10.8808 2.64711 9.94608C2.64711 9.01132 3.40488 8.25355 4.33963 8.25355H7.3109V4.85859C7.3109 3.92384 8.06867 3.16607 9.00343 3.16607C9.93819 3.16607 10.696 3.92384 10.696 4.85859V8.25355H14.0717C15.0064 8.25355 15.7642 9.01132 15.7642 9.94608C15.7642 10.8808 15.0064 11.6386 14.0717 11.6386H10.696V14.5906C10.696 15.5254 9.93819 16.2832 9.00343 16.2832Z" fill="#B7B7B7"/>
-                                            </svg>
-                                            <NuxtLink to="/products">Acelerador Biologico</NuxtLink>
-                                        </div>
-                                        <img src="../assets/image/amf_menu.svg" alt="">
-                                    </div>
-                                </MenuItem>
-
                             </div>
                         </MenuItems>
                     </Menu>    
                 </li>
-                <li><NuxtLink to="/about">Cadastra-se</NuxtLink></li>
-                <li><NuxtLink to="/products">Onde comprar</NuxtLink></li>
-                <li><NuxtLink to="/products">Nossa História</NuxtLink></li>
+                <li>
+                    <Menu v-slot="{ open }">
+                        <MenuButton  class="" :class="[ open ? 'bg-blue-500 hover:no-underline ' : '', 'hover:underline underline-offset-4 p-1 rounded-t']">Cadastre-se</MenuButton>
+                        <menuItems class="absolute  bg-white text-black rounded-b w-60 border-t-4 border-blue-500">
+                            <div class="flex flex-col">
+                                <MenuItem class="border hover:border-blue-500 p-2 group">
+                                    <NuxtLink to="/about">Seja distribuidor</NuxtLink>
+                                </MenuItem>
+                                <MenuItem class="border hover:border-blue-500 p-2 group">
+                                    <NuxtLink to="/about">Cadastre sua Loja</NuxtLink>
+                                </MenuItem>
+                            </div>
+                        </menuItems>
+                    </Menu>        
+                </li>
+                <li class="hover:underline underline-offset-4 p-2"><NuxtLink to="/products">Onde comprar</NuxtLink></li>
+                <li class="hover:underline underline-offset-4 p-2"><NuxtLink to="/products">Nossa História</NuxtLink></li>
             </ul>
         </nav>
         <div>
